75e99122b8faa9c9c3ccfb4084b1561a61c44bad
PeterAsenov22 <peter12345@abv.bg>
2018-09-30 19:11:49 +0300

    SIS.DemoApp added

---
+++ WebFundamentals/SIS-HttpServer/SIS/SIS.DemoApp/HomeController.cs
@@ -0,0 +1,16 @@
+﻿namespace SIS.DemoApp
+{
+    using HTTP.Enums;
+    using HTTP.Responses.Contracts;
+    using WebServer.Results;
+
+    public class HomeController
+    {
+        public IHttpResponse Index()
+        {
+            string content = "<h1>Hello, world!";
+
+            return new HtmlResult(content, HttpResponseStatusCode.Ok);
+        }
+    }
+}
